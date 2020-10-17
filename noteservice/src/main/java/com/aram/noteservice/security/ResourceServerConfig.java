2c09e69c4869e4316e726226d682cd052c9b85e7
Aram Ohanyan <aram.ohanyan@optym.com>
2020-10-17 23:52:35 +0400

    Add simple authentication management.

---
+++ noteservice/src/main/java/com/aram/noteservice/security/ResourceServerConfig.java
@@ -0,0 +1,25 @@
+package com.aram.noteservice.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+/**
+ *
+ * @author aram
+ */
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+            .authorizeRequests().anyRequest().authenticated();
+    }
+
+}
