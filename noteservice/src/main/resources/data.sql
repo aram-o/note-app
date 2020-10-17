88ffc738fb4012eb2d73033e4043405d4f76ffbd
Aram Ohanyan <aram.ohanyan@optym.com>
2020-10-17 16:55:36 +0400

    Add script to insert default users.

---
+++ noteservice/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+-- Insert default users.
+INSERT INTO note_user (email, password, create_date, update_date) VALUES ('test1@email.com', 'password', new Date(), new Date());
+INSERT INTO note_user (email, password, create_date, update_date) VALUES ('test2@email.com', 'password', new Date(), new Date());
+INSERT INTO note_user (email, password, create_date, update_date) VALUES ('test3@email.com', 'password', new Date(), new Date());
+INSERT INTO note_user (email, password, create_date, update_date) VALUES ('test4@email.com', 'password', new Date(), new Date());
+INSERT INTO note_user (email, password, create_date, update_date) VALUES ('test5@email.com', 'password', new Date(), new Date());
